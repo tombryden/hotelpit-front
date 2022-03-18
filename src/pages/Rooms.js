3e41a06f12639c7c920db80a3c26fb6cfb6ee18c
Tom Bryden <tombryden@me.com>
2022-03-18 15:46:29 +0000

    Initial room page

---
+++ src/pages/Rooms.js
@@ -0,0 +1,28 @@
+import { Container, Grid } from "@mui/material";
+
+import Navbar from "../components/Navbar";
+import RoomCard from "../components/RoomCard";
+import TitleWithProgress from "../components/TitleWithProgress";
+
+export default function Rooms() {
+  return (
+    <>
+      <Navbar />
+      <Container maxWidth="false" sx={{ paddingTop: "64px" }}>
+        <TitleWithProgress title="Choose a room" progress={33} mt={2} mb={1} />
+
+        <Grid container spacing={2}>
+          <Grid item xl={4}>
+            <RoomCard />
+          </Grid>
+          <Grid item xl={4}>
+            <RoomCard />
+          </Grid>
+          <Grid item xl={4}>
+            <RoomCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
