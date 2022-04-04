d8d0f001be46d6af26fa90d5f59ffba59414f107
Tom Bryden <tombryden@me.com>
2022-04-04 15:07:36 +0100

    Initial sign in capability linked to backend

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,14 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import SignInCard from "./SignInCard";
 
 export default function Navbar() {
   const navigate = useNavigate();
+
+  // state for sign in card visibility
+  const [signVisible, setSignVisible] = useState(false);
 
   return (
     <AppBar position="fixed">
@@ -16,6 +22,20 @@ export default function Navbar() {
         >
           HotelPit
         </Typography>
+
+        <Box ml="auto">
+          <Typography
+            sx={{ cursor: "pointer" }}
+            component="span"
+            onClick={() => {
+              // set sign in card visible to opposite of current state
+              setSignVisible(!signVisible);
+            }}
+          >
+            Sign In
+          </Typography>
+          <SignInCard visible={signVisible} />
+        </Box>
       </Toolbar>
     </AppBar>
   );
---
+++ src/components/SignInCard.js
@@ -0,0 +1,97 @@
+import { Card, Stack, TextField } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Box } from "@mui/system";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { useState } from "react";
+import LoginIcon from "@mui/icons-material/Login";
+
+// FUNCTIONS
+function signIn(username, password, setBtnLoading) {
+  setBtnLoading(true);
+
+  axios
+    .post("/user/login", { username, password })
+    .then(
+      (response) => {
+        // success user authenticated
+        console.log(response.data);
+        if (response.data.message === "Credentials incorrect") {
+          // invalid login
+        } else {
+          // login successful close window and display avatars
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+    .finally(() => {
+      setBtnLoading(false);
+    });
+}
+
+export default function SignInCard(props) {
+  const { visible } = props;
+
+  // state for text input
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // state for button loading
+  const [btnLoading, setBtnLoading] = useState(false);
+
+  return (
+    <Box>
+      {visible && (
+        <Card
+          sx={{
+            width: "300px",
+            position: "absolute",
+            right: "20px",
+            top: "60px",
+            padding: "10px",
+          }}
+        >
+          <Stack spacing={2}>
+            <TextField
+              label="Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <LoadingButton
+              variant="contained"
+              onClick={() => {
+                signIn(username, password, setBtnLoading);
+              }}
+              loading={btnLoading}
+              //   loadingPosition="start"
+              startIcon={<LoginIcon />}
+            >
+              Sign in
+            </LoadingButton>
+          </Stack>
+        </Card>
+      )}
+    </Box>
+  );
+}
+
+// prop types
+SignInCard.propTypes = {
+  visible: PropTypes.bool,
+};
+
+SignInCard.defaultProps = {
+  visible: false,
+};
